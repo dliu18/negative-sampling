e09169e3a5477564f653e92f1f8942c8f9ab2ad3
David Liu <liu.davi@northeastern.edu>
2025-03-10 11:15:05 -0400

    remove extra call

--- code/scripts/eval/sbm-extended.sh
+++ code/scripts/eval/sbm-extended.sh
@@ -79,22 +79,6 @@ do
 	--base_model="n2v" \
 	--loss_func="sg_aug" \
 	--test_set="test" \
-	--n_negative=10 \
-	--lr=0.01 \
-	--n2v_q=2 \
-	--lam=100 \
-	--seed=2020 \
-	--dataset="SBM-${ps[$i]}-${qs[$i]}" \
-	--recdim=128 \
-	--batch_size=128 \
-	--epochs=5 \
-	--classifier_epochs=3 \
-	--board_path="$path"
-
-	python main.py \
-	--base_model="n2v" \
-	--loss_func="sg_aug" \
-	--test_set="test" \
 	--n_negative=1000000000 \
 	--lr=0.01 \
 	--n2v_q=2 \
@@ -107,61 +91,61 @@ do
 	--classifier_epochs=3 \
 	--board_path="$path"
 
-	# python main.py \
-	# --base_model="line" \
-	# --loss_func="sg" \
-	# --test_set="test" \
-	# --lr=0.01 \
-	# --seed=2020 \
-	# --dataset="SBM-${ps[$i]}-${qs[$i]}" \
-	# --recdim=128 \
-	# --batch_size=128 \
-	# --epochs=5 \
-	# --classifier_epochs=3 \
-	# --board_path="$path"
+	python main.py \
+	--base_model="line" \
+	--loss_func="sg" \
+	--test_set="test" \
+	--lr=0.01 \
+	--seed=2020 \
+	--dataset="SBM-${ps[$i]}-${qs[$i]}" \
+	--recdim=128 \
+	--batch_size=128 \
+	--epochs=5 \
+	--classifier_epochs=3 \
+	--board_path="$path"
 
-	# python main.py \
-	# --base_model="line" \
-	# --loss_func="sg_aug" \
-	# --test_set="test" \
-	# --n_negative=10 \
-	# --lr=0.01 \
-	# --lam=1 \
-	# --seed=2020 \
-	# --dataset="SBM-${ps[$i]}-${qs[$i]}" \
-	# --recdim=128 \
-	# --batch_size=128 \
-	# --epochs=5 \
-	# --classifier_epochs=3 \
-	# --board_path="$path"
+	python main.py \
+	--base_model="line" \
+	--loss_func="sg_aug" \
+	--test_set="test" \
+	--n_negative=10 \
+	--lr=0.01 \
+	--lam=1 \
+	--seed=2020 \
+	--dataset="SBM-${ps[$i]}-${qs[$i]}" \
+	--recdim=128 \
+	--batch_size=128 \
+	--epochs=5 \
+	--classifier_epochs=3 \
+	--board_path="$path"
 
-	# python main.py \
-	# --base_model="line" \
-	# --loss_func="sg_aug" \
-	# --test_set="test" \
-	# --n_negative=100 \
-	# --lr=0.01 \
-	# --lam=1 \
-	# --seed=2020 \
-	# --dataset="SBM-${ps[$i]}-${qs[$i]}" \
-	# --recdim=128 \
-	# --batch_size=128 \
-	# --epochs=5 \
-	# --classifier_epochs=3 \
-	# --board_path="$path"
+	python main.py \
+	--base_model="line" \
+	--loss_func="sg_aug" \
+	--test_set="test" \
+	--n_negative=100 \
+	--lr=0.01 \
+	--lam=1 \
+	--seed=2020 \
+	--dataset="SBM-${ps[$i]}-${qs[$i]}" \
+	--recdim=128 \
+	--batch_size=128 \
+	--epochs=5 \
+	--classifier_epochs=3 \
+	--board_path="$path"
 
-	# python main.py \
-	# --base_model="line" \
-	# --loss_func="sg_aug" \
-	# --test_set="test" \
-	# --n_negative=1000000000 \
-	# --lr=0.01 \
-	# --lam=0.1 \
-	# --seed=2020 \
-	# --dataset="SBM-${ps[$i]}-${qs[$i]}" \
-	# --recdim=128 \
-	# --batch_size=128 \
-	# --epochs=5 \
-	# --classifier_epochs=3 \
-	# --board_path="$path"
+	python main.py \
+	--base_model="line" \
+	--loss_func="sg_aug" \
+	--test_set="test" \
+	--n_negative=1000000000 \
+	--lr=0.01 \
+	--lam=0.1 \
+	--seed=2020 \
+	--dataset="SBM-${ps[$i]}-${qs[$i]}" \
+	--recdim=128 \
+	--batch_size=128 \
+	--epochs=5 \
+	--classifier_epochs=3 \
+	--board_path="$path"
 done
